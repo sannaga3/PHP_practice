c2b4d7d21b7fea9eec121b05464a4fe61cedd4d8
sannaga3 <n.tetsuya02@gmail.com>
2022-02-23 22:45:33 +0900

    scssの記述練習

---
+++ scss_practice/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class='main-wrapper'>
+      <header class="flex-column">
+        <div class="title">
+          <h1>
+              ~ ~ ~   web site sample   ~ ~ ~
+          </h1>
+        </div>
+        <div class="shapes flex-row-between">
+          <div class="shape-group left flex-row">
+            <div class="square yellow"></div>
+            <div class="square green"></div>
+            <div class="square orange"></div>
+          </div>
+          <div class="shape-group right flex-row">
+            <div class="square yellow"></div>
+            <div class="square green"></div>
+            <div class="square orange"></div>
+          </div>
+        </div>
+        <nav>
+          <ul class="flex-row">
+            <li>トップ</li>
+            <li>事業内容</li>
+            <li>業務内容</li>
+            <li>お問合せ</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section class="main-contents flex-column">
+          <h2>メインコンテンツ</h2>
+          <p>メッセージ</p>
+        </section>
+        <section class="sub-contents flex-column">
+          <h3>お知らせ</h3>
+          <dl>
+            <dt>2022.✖️.✖️✖️</dt>
+            <dd>サイトリニューアル</dd>
+            <dt>2022.✖️.✖️✖️</dt>
+            <dd>展示会に出店します</dd>
+            <dt>2022.✖️.✖️✖️</dt>
+            <dd>夏季休暇のお知らせ</dd>
+            <dt>2022.✖️.✖️✖️</dt>
+            <dd>資本金増殖</dd>
+            <dt>2022.✖️.✖️✖️</dt>
+            <dd>冬季休暇のお知らせ</dd>
+          </dl>
+        </section>
+      </main>
+      <footer>
+        © 2022 sannaga
+      </footer>
+    </div>
+  </body>
+</html>
