74cbcdee213a8a58b0e98d0398fcc2b6b8906e3d
sannaga3 <n.tetsuya02@gmail.com>
2022-02-24 22:00:32 +0900

    poll_appのviewを個別に作成

--- scss_practice/index.php
+++ scss_practice/index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
     <title>Document</title>
   </head>
@@ -50,15 +51,19 @@
             <dt>2022.✖️.✖️✖️</dt>
             <dd>夏季休暇のお知らせ</dd>
             <dt>2022.✖️.✖️✖️</dt>
-            <dd>資本金増殖</dd>
+            <dd>資本金増資</dd>
             <dt>2022.✖️.✖️✖️</dt>
             <dd>冬季休暇のお知らせ</dd>
           </dl>
+          <button type="button" class="btn btn-info mb-1">一覧</button>
         </section>
       </main>
       <footer>
         © 2022 sannaga
       </footer>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
